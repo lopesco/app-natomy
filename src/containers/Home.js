0d18e4febc61eb6baca15b8fa9df74dc07f0c7d9
Cristiano Oliveira Lopes <39673976+lopesco@users.noreply.github.com>
2018-11-05 11:57:14 -0300

    Add files via upload

---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div className= "Home">
+                <div className= "lander">
+                    <h1>Appnatomy</h1>
+                    <p>Estudo prático de anatomia</p>
+                </div>
+            </div>
+        )
+    }
+}
